c1b17abc492d1432c349c976b50945ece2a14c1e
Jelkf <74270753+Jelkf@users.noreply.github.com>
2023-06-26 00:13:07 -0500

    4spin.py

--- Sphere.py
+++ Sphere.py
@@ -1,12 +1,11 @@
-t0 = System.currentTimeMillis()
-size = 512
-ip = FloatProcessor(size,size)
-for y in range(size):
-   IJ.showProgress(y,size-1)
-   for x in range(size):
-       dx=x-size/2; dy=y-size/2
-       d = Math.sqrt(dx*dx+dy*dy)
-       ip.setf(x,y,-d)
-time = str((System.currentTimeMillis()-t0)/1000.0)+" seconds"
-ImagePlus(time,ip).show()
-IJ.run("Red/Green");
+from dwave.system import DWaveSampler, EmbeddingComposite
+sampler = EmbeddingComposite(DWaveSampler())
+
+J = -1
+
+spins_l = {('x1', 'x1'): 1, ('x2', 'x2'): 1, ('x3', 'x3'): 1, ('x4', 'x4'): 1}
+spins_q = {('x1', 'x2'): J, ('x2', 'x3'): J, ('x3', 'x4'): J, ('x4', 'x1'): J}
+sp = {**spins_l, **spins_q}
+spins_samples = sampler.sample_ising(spins_l, spins_q, num_reads=500)
+
+print(spins_samples)
